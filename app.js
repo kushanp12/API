b6594cd44381ef82df1311e0cba96cc75b7f31c7
kushp12 <kushanp12@gmail.com>
2014-09-27 17:18:33 -0400

    Width_of_#photos

--- app.js
+++ app.js
@@ -12,15 +12,14 @@ $(document).ready(function(){
     $(".answer-getter").on("click", function(event) {
     event.preventDefault();
     $('#answer-getter').hide();
-    console.log("Submit Button Clicked");
-    getPhotos();
+    getGram();
     $("#photos").fadeIn(2000);
 	});
 
 });
 
 
-var getPhotos = function() {
+var getGram = function() {
 
   var result = $.ajax({
     url: "https://api.instagram.com/v1/tags/joking/media/recent?access_token=178445065.6c27c61.446d6cfa1bfa4246a27c2280f9cb6e76&callback=callbackFunction",
@@ -31,13 +30,9 @@ var getPhotos = function() {
   
   .done(function(result){
 
-    console.log("success!");
-
       for(var i = 0; i < 100; i++) {
 
-      
-
-     var showPhoto = function() {
+     var showGram = function() {
 
           var photo = $("#fringe").clone();
 
@@ -48,7 +43,7 @@ var getPhotos = function() {
         }
 
 
-      $("#photos").append(showPhoto);
+      $("#photos").append(showGram);
     }
   }
 
