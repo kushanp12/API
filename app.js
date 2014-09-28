279d9dbe4096db681a4bcf418727631e7d9194c8
kushp12 <kushanp12@gmail.com>
2014-09-28 16:03:48 -0400

    Comments

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 $(document).ready(function(){
+	/* When the Why Laugh button is clickes everything within container gets hidden and #about fades in */
 	$('.desc').click(function(e){
 		$('.container, .button-box, header, .holder').hide();
 		$('#about').fadeIn('slow');
 		e.preventDefault();
 	})
+
+	/* Close out button closes #about and fades in .container */
+
 	$('.x').click(function(e){
 		$('#about').hide();
 		$('.container, .button-box, header, .holder').fadeIn('slow');
 		e.preventDefault();
 	})
+
+	/* When <button> gets clicked, hides the button itself and fades in #photos */
+
     $(".answer-getter").on("click", function(event) {
     event.preventDefault();
     $('#answer-getter').hide();
     getGram();
     $("#photos").fadeIn(2000);
 	});
-
 });
 
 
 var getGram = function() {
+
+	/* var result makes a 'GET' ajax call to the IG API with data type being JSONP */
 
   var result = $.ajax({
     url: "https://api.instagram.com/v1/tags/joking/media/recent?access_token=178445065.6c27c61.446d6cfa1bfa4246a27c2280f9cb6e76&callback=callbackFunction",
@@ -30,11 +38,13 @@ var getGram = function() {
   
   .done(function(result){
 
-      for(var i = 0; i < 100; i++) {
+  	/* The function showGram goes through 20 images of IG and attributes src and an ID to the img in #laugh */ 
 
-     var showGram = function() {
+     for(var i = 0; i <= 20; i++) {
 
-          var photo = $("#fringe").clone();
+     	var showGram = function() {
+
+          var photo = $("#laugh").clone();
 
           photo.attr('src', result.data[i].images.thumbnail.url);
           photo.attr('id', result.data[i].images.thumbnail.url);
@@ -42,13 +52,11 @@ var getGram = function() {
           return photo;
         }
 
-
-      $("#photos").append(showGram);
+      /* Appends the photos from the IG API into #photos */
+	$("#photos").append(showGram);
     }
   }
-
 )
-
 }
 
 
